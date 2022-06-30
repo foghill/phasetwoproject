b9c16a976d3064e5c222372d8d91968f8b1ee0ac
Benjamin <ben@thefoghill.com>
2022-06-30 17:43:41 -0400

    some css changes

--- src/App.js
+++ src/App.js
@@ -45,7 +45,11 @@ function App() {
           </div>
         </div>
       </div>
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
 }
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,24 +1,42 @@
 import React from "react";
+import ReactPaginate from "react-paginate";
 
-const Pagination = ({ pageNumber, setPageNumber }) => {
-  let next = () => {
-    setPageNumber((prev) => prev + 1);
-  };
-  let prev = () => {
-    if (pageNumber === 1) return;
-    setPageNumber((prev) => prev - 1);
-  };
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
+  //   let next = () => {
+  //     setPageNumber((prev) => prev + 1);
+  //   };
+  //   let prev = () => {
+  //     if (pageNumber === 1) return;
+  //     setPageNumber((prev) => prev - 1);
+  //   };
 
+  //   return (
+  //     <div className="container d-flex justify-content-center gap-5 my-5">
+  //       <button className="btn btn-primary" onClick={prev}>
+  //         Previous
+  //       </button>
+  //       <button className="btn btn-primary" onClick={next}>
+  //         Next
+  //       </button>
+  //     </div>
+  //   );
+  // };
   return (
-    <div className="container d-flex justify-content-center gap-5 my-5">
-      <button className="btn btn-primary" onClick={prev}>
-        Previous
-      </button>
-      <button className="btn btn-primary" onClick={next}>
-        Next
-      </button>
-    </div>
+    <ReactPaginate
+      className="pagination justify-content-center gap-4 my-4"
+      nextLabel="Next"
+      nextClassName="btn btn-primary"
+      previousLabel="Previous"
+      previousClassName="btn btn-primary"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      activeClassName="active"
+      onPageChange={(data) => {
+        setPageNumber(data.selected + 1);
+      }}
+      pageCount={info?.pages}
+    />
   );
-};
+}; //end of Pagination
 
 export default Pagination;
