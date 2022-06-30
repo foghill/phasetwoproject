a2fd327e0b43556e5aa99569a7439ea2c0129e0e
Benjamin <ben@thefoghill.com>
2022-06-30 17:03:56 -0400

    some search filtering and page numbering

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Search from "./components/Search/Search";
 function App() {
   //state to hold page number (default is 1)
   const [pageNumber, setPageNumber] = useState(1);
+  //state to hold search term (default is empty string)
+  const [search, setSearch] = useState("");
   //state to hold character information
   const [fetchedData, updateFetchedData] = useState([]);
   //destructure Object - results go into Cards. Info will go to Pagination
   let { info, results } = fetchedData;
   //store API link in API variable for cleaner code
-  const API = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  const API = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   // make an API request and get fresh data every time the page number changes
   useEffect(() => {
@@ -30,7 +32,8 @@ function App() {
       <h1 className="text-center my-4">
         Rick and Morty <span className="text-primary">Wiki</span>
       </h1>
-      <Search />
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
       <div className="container">
         <div className="row">
           <Filters />
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./Search.module.scss";
 
-const Search = () => {
+const Search = ({ setSearch, setPageNumber }) => {
   return (
     <form className="d-flex justify-content-center gap-4 mb-5">
       <input
+        //anything we type in the input will trigger the setSearch function and be stored in the search state
+        onChange={(e) => {
+          //when we filter the results, we want to reset the page number to 1
+          setPageNumber(1);
+          setSearch(e.target.value);
+        }}
         placeholder="Search for characters"
         type="text"
         className={styles.input}
       />
-      <button className="btn btn-primary fs-5">Search</button>
+      <button className={`${styles.btn} btn btn-primary fs-5`}>Search</button>
     </form>
   );
 };
