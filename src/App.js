07e60548cb6712b089ba6dc50d578e2bf458947e
Benjamin <ben@thefoghill.com>
2022-06-30 10:57:05 -0400

    destructure API data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import Axios from "axios";
+// import Axios from "axios";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 
 function App() {
+  //state to hold page number
   const [pageNumber, setPageNumber] = useState(1);
+  //state to hold character information
+  const [fetchedData, updateFetchedData] = useState([]);
+  //destructure Object - results go into Cards. Info will go to Pagination
+  let { info, results } = fetchedData;
+  console.log(results);
   const API = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
 
   // make an API request every time the page number changes
@@ -14,7 +20,8 @@ function App() {
     //use IIFE or Axios
     (async function () {
       let data = await fetch(API).then((res) => res.json());
-      console.log(data);
+      // console.log(data.results);
+      updateFetchedData(data);
     })();
   }, [API]);
 
