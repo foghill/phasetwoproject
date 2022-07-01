f17c795d04c1e19be797a66aa81dd26b90a503a0
Benjamin <ben@thefoghill.com>
2022-07-01 09:26:42 -0400

    filter styling

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Filters = () => {
-  return <div className="col-3">Filters</div>;
+  return (
+    <div className="col-3">
+      <div className="text-center fw-bold fs-4 mb-2">Filters</div>
+      <div
+        style={{ cursor: "pointer" }}
+        className="text-center text-primary text-decoration-underline"
+      >
+        Clear Filters
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
