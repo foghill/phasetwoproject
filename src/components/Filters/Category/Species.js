207cc30c16764007f75c67afdb5a142061c91f1d
Benjamin <ben@thefoghill.com>
2022-07-01 10:09:16 -0400

    gender,species, status accordion fix

---
+++ src/components/Filters/Category/Species.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Species = () => {
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id="headingOne">
+        <button
+          className="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseOne"
+          aria-expanded="true"
+          aria-controls="collapseOne"
+        >
+          Species
+        </button>
+      </h2>
+      <div
+        id="collapseOne"
+        className="accordion-collapse collapse show"
+        aria-labelledby="headingOne"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body d-flex flex-wrap gap-3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Species;
---
+++ src/components/Filters/FilterBTN.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FilterBTN = () => {
+  return (
+    <div>
+      <div className="form-check">
+        <input className="form-check-input x" type="radio" />
+        <label className="btn btn-outline-primary">All</label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input x" type="radio" />
+        <label className="btn btn-outline-primary">All</label>
+      </div>
+    </div>
+  );
+};
+
+export default FilterBTN;
