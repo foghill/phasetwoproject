b61bb038b5355ad12634d3878d4ff2607e4ec8cd
Benjamin <ben@thefoghill.com>
2022-07-05 12:14:12 -0400

    selecting new episode changes data

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -3,7 +3,12 @@ import React from "react";
 const InputGroup = ({ name, setId, total }) => {
   return (
     <div className="input-group mb-3">
-      <select>
+      <select
+        // change the id of the episode to be fetched by API
+        onChange={(e) => setId(e.target.value)}
+        class="form-select"
+        id={name}
+      >
         <option value="1">Choose...</option>
         {[...Array(total).keys()].map((x, index) => {
           return (
