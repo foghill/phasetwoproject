82cb8fdae6ca770c4d0b86371cd4d7c2eaf3087e
Benjamin <ben@thefoghill.com>
2022-07-05 14:23:20 -0400

    readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Rick and Morty Wikipedia
 
 ## Using the [Rick and Morty API](https://rickandmortyapi.com/)
+
+```
+Packages used are:
+
+[Bootstrap](https://getbootstrap.com/)
+
+```
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,25 +2,6 @@ import React from "react";
 import ReactPaginate from "react-paginate";
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
-  //   let next = () => {
-  //     setPageNumber((prev) => prev + 1);
-  //   };
-  //   let prev = () => {
-  //     if (pageNumber === 1) return;
-  //     setPageNumber((prev) => prev - 1);
-  //   };
-
-  //   return (
-  //     <div className="container d-flex justify-content-center gap-5 my-5">
-  //       <button className="btn btn-primary" onClick={prev}>
-  //         Previous
-  //       </button>
-  //       <button className="btn btn-primary" onClick={next}>
-  //         Next
-  //       </button>
-  //     </div>
-  //   );
-  // };
   return (
     <ReactPaginate
       className="pagination justify-content-center gap-4 my-4"
