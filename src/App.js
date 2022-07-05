df4164857539e110dd4cfecd12263f469782f59b
Benjamin <ben@thefoghill.com>
2022-07-05 13:38:44 -0400

    added locaiton and episode ID routes in app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Episodes from "./Pages/Episodes";
 import Location from "./Pages/Location";
+import CardDetails from "./components/Cards/CardDetails";
 
 const App = () => {
   return (
@@ -17,8 +18,11 @@ const App = () => {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
         <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<CardDetails />} />
         <Route path="/location" element={<Location />} />
+        <Route path="/location/:id" element={<CardDetails />} />
       </Routes>
     </Router>
   );
