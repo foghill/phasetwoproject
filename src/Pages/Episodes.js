ce2ada24ca27669ef83b76ea8eabb917a71b9fbd
Benjamin <ben@thefoghill.com>
2022-07-05 10:58:28 -0400

    episode fetch IIFE

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Episodes = () => {
+  const [episodes, setEpisodes] = useState([]);
+  const [id, setId] = useState(1);
+  const API = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    //use IIFE or Axios
+    (async function () {
+      let data = await fetch(API).then((res) => res.json());
+      setEpisodes(data);
+      console.log(data);
+    })();
+  }, [API]);
+
   return <div>Episodes are here</div>;
 };
 
